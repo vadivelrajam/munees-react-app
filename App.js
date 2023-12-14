0627ca064176cdddf9df814a0ae59d5cef4ab541
Muneeswari Ganesan <muneeswariganesan211@gmail.com>
2023-12-14 12:28:41 +0000

    reinitialized commits

---
+++ App.js
@@ -0,0 +1,6 @@
+   
+    const heading = React.createElement("h1",{},"I am from react app");
+    const root = ReactDom.createRoot(document.getElementById("root"));
+    root.render(heading);
+  
+   
